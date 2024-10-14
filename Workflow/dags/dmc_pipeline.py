f9f9347c9dade7736f786a20ed6e680166c469cf
jjara2374 <jjaraa1@upao.edu.pe>
2024-10-13 23:03:03 -0500

    Update dmc_pipeline.py

--- Workflow/dags/dmc_pipeline.py
+++ Workflow/dags/dmc_pipeline.py
@@ -2,7 +2,6 @@ import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
